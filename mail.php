692ffea8f6aaed68105b3742985b87b0e176c344
Ghadeer Abbadi <40427976+GhadeerAbb@users.noreply.github.com>
2020-06-26 18:59:03 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,16 +1,16 @@
 <?php
-if(isset( $_POST['submit'])){
-	$name = $_POST['name'];
-	$emailFrom = $_POST['email'];
-	$message = $_POST['message'];
-	$subject = $_POST['subject'];
-	
-	$mailTo = "ghadeer.abbadi122@gmail.com";
-	$mailheader = "From: $emailFrom \r\n";
-	$content="From: $name \nEmail: $emailFrom \nMessage: $message";
 
-mail($mailTo, $subject, $content, $mailheader);
-	header("Location: index.html?mailsend");
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+$Name = $_POST['name'];
+$Email = $_POST['email'];
+$Phone = $_POST['subject'];
+$Message = $_POST['message'];
+
+$to = "ghadeer.abbadi122@gmail.com";
+mail($to, $Name, $Email, $Phone, $Message);
+	echo"done";
 }
 
 ?>
+
